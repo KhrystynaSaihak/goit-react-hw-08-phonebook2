27136221fcb664c35cbbf2462882ce1bb650c4ef
KhrystynaSaihak <113846974+KhrystynaSaihak@users.noreply.github.com>
2023-04-10 10:05:13 +0000

    Deploying to gh-pages from @ KhrystynaSaihak/goit-react-hw-08-phonebook2@b72be69b415c44df3a8d5538ed53e213ddd62371 🚀

--- src/components/RestrictedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
-import { selectIsLoggedIn } from 'redux/auth/selectors';
-
-export const RestrictedRoute = ({ redirectTo = '/', component: Component }) => {
-  const isLoggenIn = useSelector(selectIsLoggedIn);
-  return isLoggenIn ? <Navigate to={redirectTo} /> : Component;
-};
--- src/redux/auth/selectors.jsx
+++
@@ -1,5 +0,0 @@
-export const selectUser = state => state.auth.user;
-export const selectIsLoggedIn = state => state.auth.isLoggedIn;
-export const selectIsLogging = state => state.auth.isLogging;
-export const selectIsRefreshing = state => state.auth.isRefreshing;
-
